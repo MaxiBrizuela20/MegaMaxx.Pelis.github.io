6914b0fa362e86b3b05539a695dc3fc159560fd6
MaxiBrizuela20 <159194573+MaxiBrizuela20@users.noreply.github.com>
2024-02-06 18:31:44 -0300

    Initial commit

---
+++ css/Js/secundario.js
@@ -0,0 +1,204 @@
+let loadmorebtn1 = document.querySelector('#load-more-1')
+let currentitm1 = 4;
+
+loadmorebtn1.onclick = () => {
+    let boxes = [...document.querySelectorAll (
+        '.box-container-1 .box-1'
+    )];
+    for(var i = currentitm1; i < currentitm1 + 4; i++) {
+        boxes[i].style.display = 'inline-block'
+    }
+    currentitm1 += 4;
+    if(currentitm1 >= boxes.length) {
+        loadmorebtn1.style.display = 'none'
+    }
+    
+}
+
+let loadmorebtn2 = document.querySelector('#load-more-2')
+let currentitm2 = 4;
+
+loadmorebtn2.onclick = () => {
+    let boxes = [...document.querySelectorAll (
+        '.box-container-2 .box-2'
+    )];
+    for(var i = currentitm2; i < currentitm2 + 4; i++) {
+        boxes[i].style.display = 'inline-block'
+    }
+    currentitm2 += 4;
+    if(currentitm2 >= boxes.length) {
+        loadmorebtn2.style.display = 'none'
+    }
+    
+}
+
+let loadmorebtn3 = document.querySelector('#load-more-3')
+let currentitm3 = 4;
+
+loadmorebtn3.onclick = () => {
+    let boxes = [...document.querySelectorAll (
+        '.box-container-3 .box-3'
+    )];
+    for(var i = currentitm3; i < currentitm3 + 4; i++) {
+        boxes[i].style.display = 'inline-block'
+    }
+    currentitm3 += 4;
+    if(currentitm3 >= boxes.length) {
+        loadmorebtn3.style.display = 'none'
+    }
+    
+}
+
+
+let loadmorebtn4 = document.querySelector('#load-more-4')
+let currentitm4 = 4;
+
+loadmorebtn4.onclick = () => {
+    let boxes = [...document.querySelectorAll (
+        '.box-container-4 .box-4'
+    )];
+    for(var i = currentitm4; i < currentitm4 + 4; i++) {
+        boxes[i].style.display = 'inline-block'
+    }
+    currentitm4 += 4;
+    if(currentitm4 >= boxes.length) {
+        loadmorebtn4.style.display = 'none'
+    }
+    
+}
+
+let loadmorebtn5 = document.querySelector('#load-more-5')
+let currentitm5 = 4;
+
+loadmorebtn5.onclick = () => {
+    let boxes = [...document.querySelectorAll (
+        '.box-container-5 .box-5'
+    )];
+    for(var i = currentitm5; i < currentitm5 + 4; i++) {
+        boxes[i].style.display = 'inline-block'
+    }
+    currentitm5 += 4;
+    if(currentitm5 >= boxes.length) {
+        loadmorebtn5.style.display = 'none'
+    }
+    
+}
+
+let loadmorebtn6 = document.querySelector('#load-more-6')
+let currentitm6 = 4;
+
+loadmorebtn6.onclick = () => {
+    let boxes = [...document.querySelectorAll (
+        '.box-container-6 .box-6'
+    )];
+    for(var i = currentitm6; i < currentitm6 + 4; i++) {
+        boxes[i].style.display = 'inline-block'
+    }
+    currentitm6 += 4;
+    if(currentitm6 >= boxes.length) {
+        loadmorebtn6.style.display = 'none'
+    }
+    
+}
+
+
+let loadmorebtn7 = document.querySelector('#load-more-7')
+let currentitm7 = 4;
+
+loadmorebtn7.onclick = () => {
+    let boxes = [...document.querySelectorAll (
+        '.box-container-7 .box-7'
+    )];
+    for(var i = currentitm7; i < currentitm7 + 4; i++) {
+        boxes[i].style.display = 'inline-block'
+    }
+    currentitm7 += 4;
+    if(currentitm7 >= boxes.length) {
+        loadmorebtn7.style.display = 'none'
+    }
+    
+}
+
+let loadmorebtn8 = document.querySelector('#load-more-8')
+let currentitm8 = 4;
+
+loadmorebtn8.onclick = () => {
+    let boxes = [...document.querySelectorAll (
+        '.box-container-8 .box-8'
+    )];
+    for(var i = currentitm8; i < currentitm8 + 4; i++) {
+        boxes[i].style.display = 'inline-block'
+    }
+    currentitm8 += 4;
+    if(currentitm8 >= boxes.length) {
+        loadmorebtn8.style.display = 'none'
+    }
+    
+}
+
+
+let loadmorebtn9 = document.querySelector('#load-more-9')
+let currentitm9 = 4;
+
+loadmorebtn9.onclick = () => {
+    let boxes = [...document.querySelectorAll (
+        '.box-container-9 .box-9'
+    )];
+    for(var i = currentitm9; i < currentitm9 + 4; i++) {
+        boxes[i].style.display = 'inline-block'
+    }
+    currentitm9 += 4;
+    if(currentitm9 >= boxes.length) {
+        loadmorebtn9.style.display = 'none'
+    }
+    
+}
+
+let loadmorebtn10 = document.querySelector('#load-more-10')
+let currentitm10 = 4;
+
+loadmorebtn10.onclick = () => {
+    let boxes = [...document.querySelectorAll (
+        '.box-container-10 .box-10'
+    )];
+    for(var i = currentitm10; i < currentitm10 + 4; i++) {
+        boxes[i].style.display = 'inline-block'
+    }
+    currentitm10 += 4;
+    if(currentitm10 >= boxes.length) {
+        loadmorebtn10.style.display = 'none'
+    } 
+}
+
+
+let loadmorebtn11 = document.querySelector('#load-more-11')
+let currentitm11 = 4;
+
+loadmorebtn11.onclick = () => {
+    let boxes = [...document.querySelectorAll (
+        '.box-container-11 .box-11'
+    )];
+    for(var i = currentitm11; i < currentitm11 + 4; i++) {
+        boxes[i].style.display = 'inline-block'
+    }
+    currentitm11 += 4;
+    if(currentitm11 >= boxes.length) {
+        loadmorebtn11.style.display = 'none'
+    } 
+}
+
+let loadmorebtn12 = document.querySelector('#load-more-12')
+let currentitm12 = 4;
+
+loadmorebtn12.onclick = () => {
+    let boxes = [...document.querySelectorAll (
+        '.box-container-12 .box-12'
+    )];
+    for(var i = currentitm12; i < currentitm12 + 4; i++) {
+        boxes[i].style.display = 'inline-block'
+    }
+    currentitm12 += 4;
+    if(currentitm12 >= boxes.length) {
+        loadmorebtn12.style.display = 'none'
+    } 
+}
